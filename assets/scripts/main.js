559b75c4713c4ac263f85ccaa6464c0749896a67
Patrick G P <patrickgpavesi@gmail.com>
2018-08-09 17:03:35 -0400

    Added the ability to close out the main page terminal. Tweaked and cleaned up minor styles across the site

---
+++ assets/scripts/main.js
@@ -0,0 +1,9 @@
+const skillsTerminal = document.querySelector("#skills-terminal");
+const exitIcon = document.querySelector("#terminal-exit-icon");
+exitIcon.addEventListener(
+  "click",
+  function(e) {
+    skillsTerminal.remove();
+  },
+  false
+);
